387d04381db587a083bedaf7ac57fb3dfe2d4d07
james <jameslin809@gmail.com>
2025-01-18 14:02:12 -0500

    Resolved Previous Errors

--- goweb-spring/src/main/java/com/example/goweb_spring/repositories/MoveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.goweb_spring.repositories;
-
-import com.example.goweb_spring.entities.MoveEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MoveRepository extends JpaRepository<MoveEntity, Long> {
-    List<MoveEntity> findByGameRoom_RoomIdOrderByMoveNumber(String roomId); 
-}
